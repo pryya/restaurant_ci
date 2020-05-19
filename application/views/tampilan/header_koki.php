ed9a18136384055c6df70411b14b682231c469cc
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:47:35 +0700

    Finishin

--- application/views/tampilan/header_koki.php
+++ application/views/tampilan/header_koki.php
@@ -11,12 +11,28 @@
 
   <title>Administrator-<?=$_SESSION['nama_user']?></title>
 
-  <!-- Custom fonts for this template-->
+  <link href="<?php echo base_url()?>assets/search.css" rel="stylesheet" type="text/css">
+
   <link href="<?php echo base_url()?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
+  <!-- Custom styles for this template -->
   <link href="<?php echo base_url()?>assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this page -->
+  <link href="<?php echo base_url()?>assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+  <link href="http://cdn.jsdelivr.net/timepicker.js/latest/timepicker.min.css" rel="stylesheet"/>
+
+  <style type="text/css">
+  @media print{
+    #diprint{
+      display:none;
+    }
+  }
+  </style>
+  
+  <script type="text/javascript" src="<?php echo base_url().'assets/js/jquery-3.4.1.min.js'?>"></script>
 
 </head>
 
@@ -30,18 +46,18 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-store"></i>
+        <div class="sidebar-brand-icon rotate-n-10">
+          <i class="fas fa-store" id="diprint"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Restoran</div>
+        <div class="sidebar-brand-text mx-3" id="diprint">Restoran</div>
       </a>
 
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="">
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active" id="diprint">
+        <a class="nav-link" href="<?php echo base_url('koki/welcome') ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
@@ -54,12 +70,9 @@
           <i class="fa fa-book"></i>
           <span>Pesanan</span></a>
       </li>
-
-
-  
-
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url('auth/logout'); ?>">
+      
+      <li class="nav-item active" id="diprint">
+        <a class="nav-link" onclick="return confirm('Apakah yakin akan keluar dari sistem?')" href="<?php echo base_url('auth/logout'); ?>">
           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
           <span>Logout</span></a>
       </li>
@@ -78,67 +91,27 @@
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+     <!-- Main Content -->
+     <div id="content">
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+     <!-- Topbar -->
+     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+     <!-- Sidebar Toggle (Topbar) -->
+     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
-          </button>
+     </button>
 
-          <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-            <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-              <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                  <i class="fas fa-search fa-sm"></i>
-                </button>
-              </div>
-            </div>
-          </form>
-
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-
-
-           
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['nama_user']?></span>
-                <img class="img-profile rounded-circle" src="">
+                <!-- <img class="img-profile rounded-circle" src=""> -->
               </a>
-             
             </li>
-
           </ul>
-
         </nav>
         <!-- End of Topbar -->
+
