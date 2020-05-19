b9b72c1ae74529998bf2f921985a261d89fcf514
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:51:08 +0700

    Finishing

--- application/views/welcome.php
+++ application/views/welcome.php
@@ -1,24 +1,24 @@
+ <!-- Begin Page Content -->
+ <div class="container-fluid">
 
-    <!-- Breadcrumbs-->
-    <ol class="breadcrumb">
+ <!-- Breadcrumbs-->
+ <ol class="breadcrumb">
       <li class="breadcrumb-item">
         <a href="#">Dashboard</a>
       </li>
       <li class="breadcrumb-item active">Overview</li>
-    </ol>
+  </ol>
 
-    <!-- Area Chart Example-->
-    <div class="card mb-3">
-      <div class="card-header">
-        <h1>Selamat datang <?=$this->session->userdata('nama_user'); ?></h1>
-        <h5>Anda masuk sebagai<i> <?=$this->session->userdata('nama_level'); ?></i></h5>
-        <h4>Halaman ini hanya bisa diakses setelah login.</h4>
-      </div>
+  <!-- Area Chart Example-->
+  <div class="card mb-3">
+    <div class="card-header">
+      <h1>Selamat datang <?=$this->session->userdata('nama_user'); ?></h1>
+      <h5>Anda masuk sebagai<i> <?=$this->session->userdata('nama_level'); ?></i></h5>
+      <h4>Halaman ini hanya bisa diakses setelah login.</h4>
     </div>
+  </div>
 
-    <div class="container-fluid">
-
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+  <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
@@ -63,35 +63,8 @@
     <span class="sr-only">Next</span>
   </a>
 </div>
+</div>
+<!-- /.container-fluid -->
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title" id="exampleModalLabel">Masukan No Meja</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <?php echo form_open_multipart('admin/tambah_meja'); ?>
-
-        <div class="form-group">
-            <label>No Meja</label>
-            <input type="text" id="no_meja" name="no_meja" class="form-control" placeholder="No Meja....">
-          </div>
-
-          <div class="form-group">
-            <label>Deskripsi Meja</label>
-            <input type="text" id="deskripsi_meja" name="deskripsi_meja" class="form-control" placeholder="Deskripsi Meja....">
-          </div>
-
-         
-           <button type="reset" class="btn btn-danger" data-dismiss="modal">Reset</button>
-        <button type="submit" class="btn btn-primary">Simpan</button>
-          
-        <?php echo form_close(); ?>
-      </div>
-    </div>
-  </div>
+</div>
+<!-- End of Main Content -->
