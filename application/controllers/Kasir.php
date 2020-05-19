78dde63807b4af4c7efc630552c102fc6e661b25
pryyahaseny <nnpryya@gmail.com>
2020-05-19 19:53:21 +0700

    Finishing

--- application/controllers/Kasir.php
+++ application/controllers/Kasir.php
@@ -17,8 +17,8 @@ class Kasir extends MY_Controller {
     $this->load->view('tampilan/footer');
   }
 
-   //MENAMPILKAN DATA TEMPORY
-   public function pesanan(){
+  //MENAMPILKAN DATA TEMPORY
+  public function pesanan(){
     $data['pesan']= $this->UserModel->tampil_datapesanan()->result();
     //kode diatas ditulis jika akan menampilkan tabel dibawahnya,
     //Tetapi kalau tidak ada bisa dihilangkan
@@ -27,8 +27,8 @@ class Kasir extends MY_Controller {
     $this->load->view('tampilan/footer');
   }
 
-   //KE MODEL UNTUK SIMPAN DATA KE TABEL TEMPORARY
-   public function pesanan_tambah(){
+  //KE MODEL UNTUK SIMPAN DATA KE TABEL TEMPORARY
+  public function pesanan_tambah(){
     $p['id_pesanan']=$this->UserModel->input_pesanan();
     $data['pesan']= $this->UserModel->tampil_datapesanan()->result();
     $this->load->view('tampilan/header_kasir.php', $data);
@@ -36,25 +36,25 @@ class Kasir extends MY_Controller {
 		$this->load->view('tampilan/footer.php');
     }
 
-     //KE MODEL UNTUK MEMINDAHKAN DATA TEMPORARY KE TABEL BIASA
-     public function tambah_pesanan(){
-      $this->UserModel->pindahTemp($data, 'pesanan');
+  //KE MODEL UNTUK MEMINDAHKAN DATA TEMPORARY KE TABEL BIASA
+  public function tambah_pesanan(){
+    $this->UserModel->pindahTemp($data, 'pesanan');
     $this->session->set_flashdata('message','<div class="alert alert-success alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     Data Berhasil Ditambahkan!
-  </div>');
+    </div>');
     redirect('kasir/pesanan');
-      }
+  }
 
-    ///MENAMPILKAN HALAMAN DATA MENU
-    public function transaksi(){
-      $data['transaksi']= $this->UserModel->tampil_datatransaksi()->result();
-      $this->load->view('tampilan/header_kasir');
-      $this->load->view('kasir/transaksi',$data);
-      $this->load->view('tampilan/footer');
-    }  
+  ///MENAMPILKAN HALAMAN TRANSAKSI
+  public function transaksi(){
+    $data['transaksi']= $this->UserModel->tampil_datatransaksi()->result();
+    $this->load->view('tampilan/header_kasir');
+    $this->load->view('kasir/transaksi',$data);
+    $this->load->view('tampilan/footer');
+  }  
 
-     //PENCARIAN DATA PESANAN UNTUK TRANSAKSI
+  //PENCARIAN DATA PESANAN UNTUK TRANSAKSI
   public function search_transaksi(){
     $keyword = $this->input->get('cari_transaksi', TRUE);
     $data['transaksi'] = $this->UserModel->cari_transaksi($keyword);
@@ -64,18 +64,16 @@ class Kasir extends MY_Controller {
     $this->session->set_flashdata('flash_sukses', '<div class="alert alert-success alert-dismissible" role="alert">
     <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
     Data Berhasil Diubah!
-  </div>');
+    </div>');
   }
 
-    public function tambah_transaksi(){
-      $this->UserModel->input_transaksi($data, 'posisi_meja');
-      $this->session->set_flashdata('message','<div class="alert alert-success alert-dismissible" role="alert">
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-      Data Berhasil Ditambahkan!
+  public function tambah_transaksi(){
+    $this->UserModel->input_transaksi($data, 'posisi_meja');
+    $this->session->set_flashdata('message','<div class="alert alert-success alert-dismissible" role="alert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    Data Berhasil Ditambahkan!
     </div>');
-      redirect('kasir/transaksi');
+    redirect('kasir/transaksi');
       
-    }
-
-  
+  }
 }
