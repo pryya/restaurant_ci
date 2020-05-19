551aa7df569f40bbd5ecea12e2123478fc3bde37
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:46:16 +0700

    Finishing

--- application/views/tampilan/header.php
+++ application/views/tampilan/header.php
@@ -33,7 +33,7 @@
   </style>
   
   <script type="text/javascript" src="<?php echo base_url().'assets/js/jquery-3.4.1.min.js'?>"></script>
- 
+
 </head>
 
 <body id="page-top">
@@ -45,18 +45,18 @@
     <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
       <!-- Sidebar - Brand -->
-      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html" id="diprint">
-        <div class="sidebar-brand-icon rotate-n-20" id="diprint">
-          <i class="fas fa-store"></i>
+      <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-10">
+          <i class="fas fa-store" id="diprint"></i>
         </div>
         <div class="sidebar-brand-text mx-3" id="diprint">Restoran</div>
       </a>
 
       <!-- Divider -->
-      <hr class="sidebar-divider my-0" id="diprint">
+      <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active" id="diprint">
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active" id="diprint">
         <a class="nav-link" href="<?php echo base_url('admin/welcome') ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
@@ -65,7 +65,7 @@
      <!-- Divider -->
      <hr class="sidebar-divider my-0" id="diprint">
 
-    <!-- Nav Item - Pages Collapse Menu -->
+     <!-- Nav Item - Pages Collapse Menu -->
     <li class="nav-item" id="diprint">
         <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#master" aria-expanded="true" aria-controls="collapsePages">
           <i class="fas fa-fw fa-folder"></i>
@@ -75,8 +75,8 @@
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Data Master</h6>
             <a class="collapse-item" href="<?php echo base_url('admin/pengguna') ?>">Registrasi</a>
-            <a class="collapse-item"  href="<?php echo base_url('admin/makanan') ?>">Entri Referensi</a>
-            <a class="collapse-item"  href="<?php echo base_url('admin/meja') ?>">Meja</a>
+            <a class="collapse-item"  href="<?php echo base_url('admin/makanan') ?>">Daftar Menu</a>
+            <a class="collapse-item"  href="<?php echo base_url('admin/meja') ?>">Daftar No Meja</a>
           </div>
         </div>
       </li>
@@ -122,11 +122,11 @@
       </li>
 
       <!-- Divider -->
-      <hr class="sidebar-divider d-none d-md-block" id="diprint">
+      <hr class="sidebar-divider d-none d-md-block">
 
       <!-- Sidebar Toggler (Sidebar) -->
       <div class="text-center d-none d-md-inline">
-        <button class="rounded-circle border-0" id="sidebarToggle" id="diprint"></button>
+        <button class="rounded-circle border-0" id="sidebarToggle"></button>
       </div>
 
     </ul>
@@ -135,55 +135,27 @@
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+     <!-- Main Content -->
+     <div id="content">
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+     <!-- Topbar -->
+     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+     <!-- Sidebar Toggle (Topbar) -->
+     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
-          </button>
+     </button>
 
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-              <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <i class="fas fa-search fa-fw"></i>
-              </a>
-              <!-- Dropdown - Messages -->
-              <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                <form class="form-inline mr-auto w-100 navbar-search">
-                  <div class="input-group">
-                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                    <div class="input-group-append">
-                      <button class="btn btn-primary" type="button">
-                        <i class="fas fa-search fa-sm"></i>
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </li>
-
-
-           
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['nama_user']?></span>
                 <!-- <img class="img-profile rounded-circle" src=""> -->
               </a>
-             
             </li>
-
           </ul>
-
         </nav>
         <!-- End of Topbar -->
+
