a36a8b814cb0062f9a4effff68cb6b3d1a0306a7
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:47:00 +0700

    Finishing

--- application/views/tampilan/header_kasir.php
+++ application/views/tampilan/header_kasir.php
@@ -11,12 +11,28 @@
 
   <title>Administrator-<?=$_SESSION['nama_user']?></title>
 
-  <!-- Custom fonts for this template-->
+  <link href="<?php echo base_url()?>assets/search.css" rel="stylesheet" type="text/css">
+
   <link href="<?php echo base_url()?>assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
-  <!-- Custom styles for this template-->
+  <!-- Custom styles for this template -->
   <link href="<?php echo base_url()?>assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this page -->
+  <link href="<?php echo base_url()?>assets/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
+
+  <link href="http://cdn.jsdelivr.net/timepicker.js/latest/timepicker.min.css" rel="stylesheet"/>
+
+  <style type="text/css">
+  @media print{
+    #diprint{
+      display:none;
+    }
+  }
+  </style>
+  
+  <script type="text/javascript" src="<?php echo base_url().'assets/js/jquery-3.4.1.min.js'?>"></script>
 
 </head>
 
@@ -30,41 +46,39 @@
 
       <!-- Sidebar - Brand -->
       <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-        <div class="sidebar-brand-icon rotate-n-15">
-          <i class="fas fa-store"></i>
+        <div class="sidebar-brand-icon rotate-n-10">
+          <i class="fas fa-store" id="diprint"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Restoran</div>
+        <div class="sidebar-brand-text mx-3" id="diprint">Restoran</div>
       </a>
 
       <!-- Divider -->
       <hr class="sidebar-divider my-0">
 
-      <!-- Nav Item - Dashboard -->
-      <li class="nav-item active">
-        <a class="nav-link" href="">
+        <!-- Nav Item - Dashboard -->
+        <li class="nav-item active" id="diprint">
+        <a class="nav-link" href="<?php echo base_url('admin/welcome') ?>">
           <i class="fas fa-fw fa-tachometer-alt"></i>
           <span>Dashboard</span></a>
       </li>
 
-     <!-- Divider -->
-     <hr class="sidebar-divider my-0">
+      <!-- Divider -->
+      <hr class="sidebar-divider my-0">
 
-     <li class="nav-item active" id="diprint">
+      <li class="nav-item active" id="diprint">
         <a class="nav-link" href="<?php echo base_url('kasir/pesanan') ?>">
-          <i class="fa fa-book"></i>
-          <span>Pesanan</span></a>
+        <i class="fa fa-book"></i>
+        <span>Pesanan</span></a>
       </li>
 
-     <li class="nav-item active" id="diprint">
+      <li class="nav-item active" id="diprint">
         <a class="nav-link" href="<?php echo base_url('kasir/transaksi') ?>">
-          <i class="fa fa-cash-register"></i>
-          <span>Transaksi</span></a>
+        <i class="fa fa-cash-register"></i>
+        <span>Transaksi</span></a>
       </li>
-
-  
-
-      <li class="nav-item active">
-        <a class="nav-link" href="<?php echo base_url('auth/logout'); ?>">
+      
+      <li class="nav-item active" id="diprint">
+        <a class="nav-link" onclick="return confirm('Apakah yakin akan keluar dari sistem?')" href="<?php echo base_url('auth/logout'); ?>">
           <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
           <span>Logout</span></a>
       </li>
@@ -83,31 +97,27 @@
     <!-- Content Wrapper -->
     <div id="content-wrapper" class="d-flex flex-column">
 
-      <!-- Main Content -->
-      <div id="content">
+     <!-- Main Content -->
+     <div id="content">
 
-        <!-- Topbar -->
-        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+     <!-- Topbar -->
+     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
-          <!-- Sidebar Toggle (Topbar) -->
-          <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+     <!-- Sidebar Toggle (Topbar) -->
+     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
             <i class="fa fa-bars"></i>
-          </button>
+     </button>
 
-          <!-- Topbar Navbar -->
-          <ul class="navbar-nav ml-auto">
-            <div class="topbar-divider d-none d-sm-block"></div>
-
+      <!-- Topbar Navbar -->
+      <ul class="navbar-nav ml-auto">
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
               <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['nama_user']?></span>
-               
-              </a>
-             
+                <!-- <img class="img-profile rounded-circle" src=""> -->
+              </a>   
             </li>
-
           </ul>
-
         </nav>
         <!-- End of Topbar -->
+
