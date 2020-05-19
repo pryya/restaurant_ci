42b45b0a46bf2fa7a53758a05214ad5f046cc95a
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:48:12 +0700

    Finishin

--- application/views/tampilan/header_owner.php
+++ application/views/tampilan/header_owner.php
@@ -137,22 +137,12 @@
                 </form>
               </div>
             </li>
-
-
-           
-
             <div class="topbar-divider d-none d-sm-block"></div>
 
             <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['nama_user']?></span>
-                <img class="img-profile rounded-circle" src="">
-              </a>
-             
+            <li class="nav-item dropdown no-arrow" aria-haspopup="true" aria-expanded="false">
+              <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?=$_SESSION['nama_user']?></span>
             </li>
-
           </ul>
-
         </nav>
         <!-- End of Topbar -->
