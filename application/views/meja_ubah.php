890461b790b9ead52b4b2ecd7d9c0a7b45a6a495
pryyahaseny <nnpryya@gmail.com>
2020-05-19 20:39:32 +0700

    Finishing

--- application/views/meja_ubah.php
+++ application/views/meja_ubah.php
@@ -9,15 +9,14 @@
              <div class="modal-body">
               <div class="form-group">
                 <input type="hidden" id="no_meja" name="no_meja" class="form-control" value="<?=$meja['no_meja'];?>">
+              
                 <label class="control-label">No Meja</label>
                 <input type="text" class="form-control" id="no_meja" name="no_meja" placeholder="No Meja" value="<?=$meja['no_meja'];?>" required readonly>
-                 <label class="control-label">Deskripsi Meja </label>
+              
+                <label class="control-label">Deskripsi Meja </label>
                 <input type="text" class="form-control" id="deskripsi_meja" name="deskripsi_meja" placeholder="Deskripsi Meja" value="<?=$meja['deskripsi_meja'];?>" required>
-                
-                 
               </div>
             </div>
-              
             <div class="modal-footer">
               <a href="<?=base_url('admin/meja');?>" type="button" class="btn btn-danger" data-dismiss="modal"><i class="fas fa-undo"></i>&nbsp;&nbsp;Batal&nbsp;</a>
               <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i>&nbsp;&nbsp;Update</button>
